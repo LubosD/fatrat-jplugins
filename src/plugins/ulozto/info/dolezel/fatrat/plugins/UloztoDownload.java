2b469f49204b6a030debbd74a99efda2dea51e93
Lubos Dolezel <lubos@dolezel.info>
2010-12-08 03:21:57 +0100

    Working Uloz.to support

--- src/plugins/ulozto/info/dolezel/fatrat/plugins/UloztoDownload.java
+++ src/plugins/ulozto/info/dolezel/fatrat/plugins/UloztoDownload.java
@@ -24,6 +24,7 @@ import info.dolezel.fatrat.plugins.listeners.CaptchaListener;
 import info.dolezel.fatrat.plugins.listeners.PageFetchListener;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -37,52 +38,62 @@ public class UloztoDownload extends DownloadPlugin {
 
     static final Pattern reImage = Pattern.compile("src=\"(http://img\\.uloz\\.to/captcha/(\\d+)\\.png)\"");
     static final Pattern reAction = Pattern.compile("<form name=\"dwn\" action=\"([^\"]+)\"");
+    final Charset charset = Charset.forName("UTF-8");
 
     @Override
     public void processLink(String link) {
+
+        if (link.contains("/live/"))
+            link = link.replace("/live/", "/");
+
         fetchPage(link, new PageFetchListener() {
 
             public void onCompleted(ByteBuffer buf, Map<String,String> headers) {
-                CharBuffer cb = buf.asCharBuffer();
-                final Matcher m = reImage.matcher(cb);
-                final Matcher mAction = reAction.matcher(cb);
+                try {
+                    CharBuffer cb = charset.decode(buf);
+                    final Matcher m = reImage.matcher(cb);
+                    final Matcher mAction = reAction.matcher(cb);
 
-                if (!m.find()) {
-                    setFailed("Failed to find the captcha code");
-                    return;
-                }
-                if (!mAction.find()) {
-                    setFailed("Failed to find the form action");
-                    return;
-                }
-
-                String captchaUrl = m.group(1);
-                UloztoDownload.this.solveCaptcha(captchaUrl, new CaptchaListener() {
-
-                    public void onFailed() {
-                        setFailed("Failed to decode the captcha code");
+                    if (!m.find()) {
+                        setFailed("Failed to find the captcha code");
+                        return;
+                    }
+                    if (!mAction.find()) {
+                        setFailed("Failed to find the form action");
+                        return;
                     }
 
-                    public void onSolved(String text) {
-                        String captchaCode = m.group(2);
-                        fetchPage(mAction.group(1), new PageFetchListener() {
+                    String captchaUrl = m.group(1);
+                    UloztoDownload.this.solveCaptcha(captchaUrl, new CaptchaListener() {
 
-                            public void onCompleted(ByteBuffer buf, Map<String, String> headers) {
-                                String target = headers.get("location");
-                                if (target == null)
-                                    setFailed("Failed to get the redirect URL");
-                                else
-                                    startDownload(target);
-                            }
+                        public void onFailed() {
+                            setFailed("Failed to decode the captcha code");
+                        }
 
-                            public void onFailed(String error) {
-                                setFailed(error);
-                            }
-                            
-                        }, "captcha_nb="+captchaCode+"&captcha_user="+text+"&download=St%C3%A1hnout%20FREE");
-                        
-                    }
-                });
+                        public void onSolved(String text) {
+                            String captchaCode = m.group(2);
+                            fetchPage(mAction.group(1), new PageFetchListener() {
+
+                                public void onCompleted(ByteBuffer buf, Map<String, String> headers) {
+                                    String target = headers.get("location");
+                                    if (target == null)
+                                        setFailed("Failed to get the redirect URL");
+                                    else
+                                        startDownload(target);
+                                }
+
+                                public void onFailed(String error) {
+                                    setFailed(error);
+                                }
+
+                            }, "captcha_nb="+captchaCode+"&captcha_user="+text+"&download=St%C3%A1hnout%20FREE");
+
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    setFailed(e.toString());
+                }
             }
 
             public void onFailed(String error) {
